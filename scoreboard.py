1ec47e1ec8d4a0e10dd98f47515a68aa907bf690
Jose Victor <josemartins2099@gmail.com>
2025-07-26 09:40:34 -0300

    first commit

---
+++ scoreboard.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color('white')
+        self.penup()
+        self.goto(0,260)
+        self.hideturtle()
+        self.write(arg=f'Score: {self.score}', move=False, align='center', font=('Arial', 24, 'normal'))
+
+
+    def increase(self):
+        self.score +=1
+        self.clear()
+        self.write(arg=f'Score: {self.score}', move=False, align='center', font=('Arial', 24, 'normal'))
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(arg=f'GAME OVER', move=False, align='center', font=('Arial', 24, 'normal'))
